fca0f10525bf70cec0847cdff04d2792eb2f06db
dbrow221 <dbrow221@charlotte.edu>
2025-04-26 12:16:42 -0400

    project files added

--- api/routers/promotions.py
+++ api/routers/promotions.py
@@ -1,8 +1,8 @@
-from fastapi import APIRouter, Depends, FastAPI, status, Response
+from fastapi import APIRouter, Depends, status
 from sqlalchemy.orm import Session
-from ..controllers import orders as controller
-from ..schemas import orders as schema
-from ..dependencies.database import engine, get_db
+from ..controllers import promotions as controller
+from ..schemas import promotions as schema
+from ..dependencies.database import get_db
 
 router = APIRouter(
     tags=['Promotions'],
@@ -20,16 +20,16 @@ def read_all(db: Session = Depends(get_db)):
     return controller.read_all(db)
 
 
-@router.get("/{item_id}", response_model=schema.Order)
+@router.get("/{item_id}", response_model=schema.Promotion)
 def read_one(item_id: int, db: Session = Depends(get_db)):
-    return controller.read_one(db, item_id=item_id)
+    return controller.read_one(db=db, promotion_code=item_id)  # <-- match controller param
 
 
-@router.put("/{item_id}", response_model=schema.Order)
-def update(item_id: int, request: schema.OrderUpdate, db: Session = Depends(get_db)):
-    return controller.update(db=db, request=request, item_id=item_id)
+@router.put("/{item_id}", response_model=schema.Promotion)
+def update(item_id: int, request: schema.PromotionUpdate, db: Session = Depends(get_db)):
+    return controller.update(db=db, promotion_code=item_id, request=request)  # <-- match controller param order
 
 
-@router.delete("/{item_id}")
+@router.delete("/{item_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete(item_id: int, db: Session = Depends(get_db)):
-    return controller.delete(db=db, item_id=item_id)
+    return controller.delete(db=db, promotion_code=item_id)  # <-- match controller param
